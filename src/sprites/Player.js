6885a4bc44cd922d7f7b83d5d4d32ff835a909a9
Tournier Valentin <valentin@macbook-pro-de-tournier.home>
2017-12-02 17:37:23 +0100

    Allow double jump

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -4,5 +4,27 @@ export default class extends Phaser.Sprite {
   constructor ({ game, x, y, asset }) {
     super(game, x, y, asset)
     this.anchor.setTo(0.5)
+
+    this.props = {
+      max_jump: 2
+    }
+
+    this.state = {
+      current_jump: 0
+    }
+  }
+
+  static jump () {
+    if (this.state.current_jump >= this.props.max_jump)
+      return
+
+    this.body.velocity.y = -500
+    this.state.current_jump++
+  }
+
+  static resetJump(player) {
+    if (player.body.velocity.y === 0) {
+      player.state.current_jump = 0
+    }
   }
 }
