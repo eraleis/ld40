75bb27bf05484aacf9161df5aab467d2defdd294
Tournier Valentin <valentin@macbook-pro-de-tournier.home>
2017-12-02 15:41:13 +0100

    Generate default world

--- src/states/Game.js
+++ src/states/Game.js
@@ -6,6 +6,29 @@ export default class extends Phaser.State {
   init () {}
   preload () {}
 
+  create_world () {
+    let map = [
+      [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0],
+      [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1]
+    ]
+
+    var pos = {x: 0, y: this.world.height};
+
+    for (var i = map.length - 1; i >= 0; i--) {
+      pos.x = 0;
+      for (var j = 0; j < map[i].length; j++) {
+        if (map[i][j] == 1) {
+          let block = this.game.add.sprite(pos.x, pos.y, 'block');
+          block.anchor.y = 1;
+        }
+        pos.x += 32;
+      }
+      pos.y -= 32;
+    }
+
+  }
+
   create () {
     const bannerText = 'Phaser + ES6 + Webpack'
     let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
