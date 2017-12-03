c5325cc33a655b1687da79a70d3c8fdb3a4fad72
eraleis <vatourni@gmail.com>
2017-12-03 20:24:40 +0100

    Fix cops ai

--- src/sprites/Cop.js
+++ src/sprites/Cop.js
@@ -22,8 +22,10 @@ export default class extends Phaser.Sprite {
 
   canMove () {
     let index = parseInt(this.x / BLOCK_SIZE)
+    let y = Math.abs(parseInt((this.y - game.height) / BLOCK_SIZE))
+    console.log(HEIGHT - y, HEIGHT - 1)
     let direction = this.body.velocity.x >= 0 ? 1 : -1
-    let next = map[HEIGHT - 1][index + direction]
+    let next = map[HEIGHT - y][index + direction]
     if (next === 0 || next === undefined) { return false }
     return true
   }
